7056e83e23d63de5a0c9a0df165679b50e47e096
RameshTanugula <gnitramesh@gmail.com>
2022-12-18 20:56:54 +0530

    updated headers

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,9 @@ export default async function api(reqObj, url, type) {
         method: type,
         headers: {
             "Authorization": "1",
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
         },
         data: reqObj,
     })
