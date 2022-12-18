0f44bed432fd52cacdd93fa09ed22e0dd69df561
RameshTanugula <gnitramesh@gmail.com>
2022-12-18 20:50:56 +0530

    headers added

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 export default async function api(reqObj, url, type) {
     return await axios({
-url:  url,
-method: type,
-headers: {
-    "Authorization":"1"
-},
-data: reqObj,
-})
-.then((res) => {
-    return res;
- })
-.catch((err) => {
-return err;
-});
+        url: url,
+        method: type,
+        headers: {
+            "Authorization": "1",
+            "Access-Control-Allow-Origin": "*"
+        },
+        data: reqObj,
+    })
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            return err;
+        });
 }
