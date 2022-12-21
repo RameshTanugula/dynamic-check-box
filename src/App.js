7c707d538aee9f1aefbe68ca0086b238edd61f01
RameshTanugula <gnitramesh@gmail.com>
2022-12-21 21:55:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
   // const tmpData = { viewData: treeViewData }
   // const result = tmpData.viewData.flatMap(flat);
   const getTagName = (id) => {
-    return result?.find(r => r.id === +id)?.label + ' X';
+    return result?.find(r => r.id === +id)?.label;
   }
 
 
@@ -193,7 +193,7 @@ export default function App() {
                   {qData.tags &&
                     qData.tags?.split(',')?.sort()?.map((tg, j) =>
                       <div style={{ paddingRight: '5px' }}>
-                        <span><button onClick={() => removeTag(tg, j, qData.q_id)}>{getTagName(tg) || ''}</button></span>
+                        <span><button onClick={() => removeTag(tg, j, qData.q_id)}>{getTagName(tg)} X</button></span>
                       </div>)}
                 </div>
               </div>)
