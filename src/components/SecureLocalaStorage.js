ee061acd6aab6e245bbb444b0221339981d849f5
RameshTanugula <gnitramesh@gmail.com>
2023-07-28 19:12:11 +0530

    changed api address

--- src/components/SecureLocalaStorage.js
+++ src/components/SecureLocalaStorage.js
@@ -15,8 +15,8 @@ export const remove = (key) => {
 }
 
 // export const baseUrl = "http://65.0.6.118:8080/";
-export const baseUrl = "http://13.200.33.182:8080/";
-export const omrScriptUrl = "http://13.200.33.182:5000/"
+export const baseUrl = "http://13.127.234.197:8080/";
+export const omrScriptUrl = "http://13.127.234.197:5000/"
 export const subjectsUrl = baseUrl + 'files/get/subjects';
 
 export const categoriesUrl = baseUrl + 'get/categories';
